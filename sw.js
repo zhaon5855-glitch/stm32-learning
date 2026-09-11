f6595cd6d9c0c5d3c0c92c057bc134f4f51fc97f
zhaon5855-glitch <zhaon5855-glitch@users.noreply.github.com>
2026-09-12 05:30:28 +0800

    更新SW缓存版本v2+改为网络优先策略，修复旧缓存问题

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stm32-learn-v1';
+const CACHE = 'stm32-learn-v2';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -15,10 +15,10 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    fetch(e.request).then(res => {
       const copy = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
       return res;
-    }).catch(() => caches.match('./index.html')))
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
   );
 });
