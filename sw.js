a8401c7fc883d9b91b4fc9893fc944d2a44a370a
zhaon5855-glitch <zhaon5855-glitch@users.noreply.github.com>
2026-09-12 05:37:52 +0800

    SW改为纯透传模式，不缓存HTML，更新即时生效

--- sw.js
+++ sw.js
@@ -1,24 +1,17 @@
-const CACHE = 'stm32-learn-v2';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+// 纯透传模式：不缓存任何内容，保证更新即时生效
+self.addEventListener('install', () => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+    Promise.all(keys.map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', e => {
+  // 只在离线时回退到缓存，正常情况下始终走网络
   e.respondWith(
-    fetch(e.request).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
-      return res;
-    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
